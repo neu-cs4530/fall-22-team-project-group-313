9079ba6c021ffceab83f3b4e1450e0f74ca8a714
Sean Mitchell <seanjm77@gmail.com>
2022-11-01 14:43:16 -0400

    Created BlackjackArea and tests

---
+++ townService/src/town/BlackjackArea.test.ts
@@ -0,0 +1,93 @@
+import { mock, mockClear } from 'jest-mock-extended';
+import { nanoid } from 'nanoid';
+import Player from '../lib/Player';
+import { getLastEmittedEvent } from '../TestUtils';
+import { TownEmitter } from '../types/CoveyTownSocket';
+import BlackjackArea from './BlackjackArea';
+
+describe('BlackjackArea', () => {
+  const testAreaBox = { x: 100, y: 100, width: 100, height: 100 };
+  let testArea: BlackjackArea;
+  const townEmitter = mock<TownEmitter>();
+  const numDecks = 6;
+  const id = nanoid();
+  let newPlayer: Player;
+
+  beforeEach(() => {
+    mockClear(townEmitter);
+    testArea = new BlackjackArea({ id, occupantsByID: [], numDecks }, testAreaBox, townEmitter);
+    newPlayer = new Player(nanoid(), mock<TownEmitter>());
+    testArea.add(newPlayer);
+  });
+  describe('add', () => {
+    it('Adds the player to the occupants list and emits an interactableUpdate event', () => {
+      expect(testArea.occupantsByID).toEqual([newPlayer.id]);
+
+      const lastEmittedUpdate = getLastEmittedEvent(townEmitter, 'interactableUpdate');
+      expect(lastEmittedUpdate).toEqual({ numDecks, id, occupantsByID: [newPlayer.id] });
+    });
+    it("Sets the player's blackjackLabel and emits an update for their location", () => {
+      expect(newPlayer.location.interactableID).toEqual(id);
+
+      const lastEmittedMovement = getLastEmittedEvent(townEmitter, 'playerMoved');
+      expect(lastEmittedMovement.location.interactableID).toEqual(id);
+    });
+  });
+  describe('remove', () => {
+    it('Removes the player from the list of occupants and emits an interactableUpdate event', () => {
+      // Add another player so that we are not also testing what happens when the last player leaves
+      const extraPlayer = new Player(nanoid(), mock<TownEmitter>());
+      testArea.add(extraPlayer);
+      testArea.remove(newPlayer);
+
+      expect(testArea.occupantsByID).toEqual([extraPlayer.id]);
+      const lastEmittedUpdate = getLastEmittedEvent(townEmitter, 'interactableUpdate');
+      expect(lastEmittedUpdate).toEqual({ id, occupantsByID: [extraPlayer.id], numDecks });
+    });
+    it("Clears the player's blackjackLabel and emits an update for their location", () => {
+      testArea.remove(newPlayer);
+      expect(newPlayer.location.interactableID).toBeUndefined();
+      const lastEmittedMovement = getLastEmittedEvent(townEmitter, 'playerMoved');
+      expect(lastEmittedMovement.location.interactableID).toBeUndefined();
+    });
+    it('Clears the numDecks of the blackjack area when the last occupant leaves', () => {
+      testArea.remove(newPlayer);
+      const lastEmittedUpdate = getLastEmittedEvent(townEmitter, 'interactableUpdate');
+      expect(lastEmittedUpdate).toEqual({ topic: undefined, id, occupantsByID: [] });
+      expect(testArea.numDecks).toBeUndefined();
+    });
+  });
+  test('toModel sets the ID, numDecks and occupantsByID and sets no other properties', () => {
+    const model = testArea.toModel();
+    expect(model).toEqual({
+      id,
+      numDecks,
+      occupantsByID: [newPlayer.id],
+    });
+  });
+  describe('fromMapObject', () => {
+    it('Throws an error if the width or height are missing', () => {
+      expect(() =>
+        BlackjackArea.fromMapObject(
+          { id: 1, name: nanoid(), visible: true, x: 0, y: 0 },
+          townEmitter,
+        ),
+      ).toThrowError();
+    });
+    it('Creates a new blackjack area using the provided boundingBox and id, with an empty occupants list', () => {
+      const x = 30;
+      const y = 20;
+      const width = 10;
+      const height = 20;
+      const name = 'name';
+      const val = BlackjackArea.fromMapObject(
+        { x, y, width, height, name, id: 10, visible: true },
+        townEmitter,
+      );
+      expect(val.boundingBox).toEqual({ x, y, width, height });
+      expect(val.id).toEqual(name);
+      expect(val.numDecks).toBeUndefined();
+      expect(val.occupantsByID).toEqual([]);
+    });
+  });
+});
---
+++ townService/src/town/BlackjackArea.ts
@@ -0,0 +1,80 @@
+import { ITiledMapObject } from '@jonbell/tiled-map-type-guard';
+import Player from '../lib/Player';
+import {
+  BoundingBox,
+  BlackjackArea as BlackjackModel,
+  TownEmitter,
+} from '../types/CoveyTownSocket';
+import InteractableArea from './InteractableArea';
+
+export default class BlackjackArea extends InteractableArea {
+  /* The number of decks set in the blackjack area, or undefined if it is not set */
+  public numDecks?: number;
+
+  /** The blackjack area is "active" when there are players inside of it  */
+  public get isActive(): boolean {
+    return this._occupants.length > 0;
+  }
+
+  /**
+   * Creates a new BlackjackArea
+   *
+   * @param BlackjackModel model containing this area's current number of decks and its ID
+   * @param coordinates  the bounding box that defines this blackjack area
+   * @param townEmitter a broadcast emitter that can be used to emit updates to players
+   */
+  public constructor(
+    { id, numDecks }: BlackjackModel,
+    coordinates: BoundingBox,
+    townEmitter: TownEmitter,
+  ) {
+    super(id, coordinates, townEmitter);
+    this.numDecks = numDecks;
+  }
+
+  /**
+   * Removes a player from this game area.
+   *
+   * Extends the base behavior of InteractableArea to set the number of decks of this GameArea
+   * to undefined and emit an update to other players in the town when the last player leaves.
+   *
+   * @param player
+   */
+  public remove(player: Player) {
+    super.remove(player);
+    if (this._occupants.length === 0) {
+      this.numDecks = undefined;
+      this._emitAreaChanged();
+    }
+  }
+
+  /**
+   * Convert this BlackjackArea instance to a simple BlackjackModel suitable for
+   * transporting over a socket to a client.
+   */
+  public toModel(): BlackjackModel {
+    return {
+      id: this.id,
+      occupantsByID: this.occupantsByID,
+      numDecks: this.numDecks,
+    };
+  }
+
+  /**
+   * Creates a new BlackjackArea object that will represent a Blackjack Area object in the town map.
+   * @param mapObject An ITiledMapObject that represents a rectangle in which this blackjack area exists
+   * @param broadcastEmitter An emitter that can be used by this blackjack area to broadcast updates
+   * @returns
+   */
+  public static fromMapObject(
+    mapObject: ITiledMapObject,
+    broadcastEmitter: TownEmitter,
+  ): BlackjackArea {
+    const { name, width, height } = mapObject;
+    if (!width || !height) {
+      throw new Error(`Malformed viewing area ${name}`);
+    }
+    const rect: BoundingBox = { x: mapObject.x, y: mapObject.y, width, height };
+    return new BlackjackArea({ id: name, occupantsByID: [] }, rect, broadcastEmitter);
+  }
+}

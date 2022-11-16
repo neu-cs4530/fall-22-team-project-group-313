65334436454362e454993100f80538d542a0f0a6
lnardini <llnardini1@gmail.com>
2022-11-16 16:20:21 -0500

    Fixed testing
    Testing almost finished
    Changed card rank to number vs enum

--- townService/src/lib/Card.ts
+++ townService/src/lib/Card.ts
@@ -5,65 +5,65 @@ export enum Suit {
   C = 'C',
 }
 
-export enum Rank {
-  Two = '2',
-  Three = '3',
-  Four = '4',
-  Five = '5',
-  Six = '6',
-  Seven = '7',
-  Eight = '8',
-  Nine = '9',
-  Ten = '10',
-  Jack = 'J',
-  Queen = 'Q',
-  King = 'K',
-  Ace = 'A',
-}
+// export enum Rank {
+//   Ace = 1,
+//   Two = 2,
+//   Three = 3,
+//   Four = 4,
+//   Five = 5,
+//   Six = 6,
+//   Seven = 7,
+//   Eight = 8,
+//   Nine = 9,
+//   Ten = 10,
+//   Jack = 11,
+//   Queen = 13,
+//   King = 14
+// }
 
 export default class Card {
   public readonly suit: Suit;
 
-  public readonly rank: Rank;
+  public readonly rank: string;
 
   public isFaceUp: boolean;
 
-  public get value() {
-    switch (this.rank) {
-      case Rank.Ace:
-        return 1;
-      case Rank.Two:
-        return 2;
-      case Rank.Three:
-        return 3;
-      case Rank.Four:
-        return 4;
-      case Rank.Five:
-        return 5;
-      case Rank.Six:
-        return 6;
-      case Rank.Seven:
-        return 7;
-      case Rank.Eight:
-        return 8;
-      case Rank.Nine:
-        return 9;
-      default:
-        return 10;
-    }
+  private _rankToValue = new Map<string, number>();
+
+  public toString(): string {
+    return this.suit + this.rank;
   }
 
-  constructor(suit: Suit, rank: Rank, isFaceUp?: boolean) {
+  private constructor(suit: Suit, rank: string) {
     this.suit = suit;
     this.rank = rank;
-    this.isFaceUp = isFaceUp ?? true;
+    this.isFaceUp = true;
+    this._rankToValue.set('Ace', 11);
+    this._rankToValue.set('Two', 2);
+    this._rankToValue.set('Three', 3);
+    this._rankToValue.set('Four', 4);
+    this._rankToValue.set('Five', 5);
+    this._rankToValue.set('Six', 6);
+    this._rankToValue.set('Seven', 7);
+    this._rankToValue.set('Eight', 8);
+    this._rankToValue.set('Nine', 9);
+    this._rankToValue.set('Ten', 10);
+    this._rankToValue.set('Jack', 10);
+    this._rankToValue.set('Queen', 10);
+    this._rankToValue.set('King', 10);
+  }
+
+  public get value() {
+    return this._rankToValue.get(this.rank) as number;
   }
 
   public static getDeck(): Card[] {
     const deck = new Array<Card>();
-    Object.keys(Suit).forEach(suit => {
-      Object.keys(Rank).forEach(rank => {
-        deck.push(new Card(suit as Suit, rank as Rank));
+    const card = new Card(Suit.C, 'Ace');
+    const ranks = Array.from(card._rankToValue.keys());
+    ranks.forEach(rank => {
+      Object.keys(Suit).forEach(suit => {
+        deck.push(new Card(suit as Suit, rank));
       });
     });
     return deck;
